67d95de060e01b51a823800c5a62e40e81b68751
ABLavine <134315075+ABLavine@users.noreply.github.com>
2023-05-22 13:00:16 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function calculateAge(birthdate) {
+    const today = new Date();
+    const birthDate = new Date(birthdate);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDiff = today.getMonth() - birthDate.getMonth();
+    
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+    console.log('Code is running');
+    return age;
+  }
+  
+  // U
+  document.addEventListener("DOMContentLoaded", function() {
+    const birthdate = "2007-02-05"; 
+    const ageElement = document.getElementById("age");
+  
+    if (ageElement) {
+        ageElement.textContent = `I'm ${calculateAge(birthdate)} years old. I was born in Cambridge MA, and currenly still live there. I love video games, baking, dogs, and helping out my friends. I love to code/learn about the way things work, and this kinda extends to my love of philosophy.
+        I want to figure out how the world works, and I think that the fusion of code and philosophy/big questions is cruical in this time of developing AI.`;    }
+  });
+
